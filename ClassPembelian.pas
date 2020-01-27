8ef8b42bb36e24cf93e0f79917a3a1b7fbf40295
FA <wishmeluck.amien@gmail.com>
2020-01-27 09:09:13 +0700

    ClassPembelian v2

--- ClassPembelian.pas
+++ ClassPembelian.pas
@@ -1,7 +1,18 @@
 unit ClassPembelian;
-
 interface
-
 implementation
-
 end.
+{Procedure
+  Buat Pembelian baru
+  Ubah Pembelian
+  Hapus Pembelian
+
+Function
+  Cari Pembelian berdasarkan
+  1 Kode Pembelian
+  2 Tanggal Pembelian
+  3 Pembeli: Kode/Nama
+  4 Barang: Kode/Nama
+
+  Ubah Pembelian yg sebelumnya diperoleh dari pencarian
+}
