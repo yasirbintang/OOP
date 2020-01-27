5004c83c72667946aa9fdcf47da0b83b740660dd
FA <wishmeluck.amien@gmail.com>
2020-01-27 09:46:18 +0700

    ClassPembelian v3

--- ClassPembelian.pas
+++ ClassPembelian.pas
@@ -1,5 +1,19 @@
 unit ClassPembelian;
+
 interface
+uses
+  uConnection, SysUtils;
+
+  type
+  tPembelian = class
+  private
+    FID: Integer;
+    FnoBukti: string;
+    Ftgl: TDateTime;
+    FPembeli: string;
+  public
+  end;
+
 implementation
 end.
 {Procedure
